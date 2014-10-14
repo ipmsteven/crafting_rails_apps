eeaf9472432db4ede631b1240c193a0f8c547626
yunlei <yunlei.liu@appfolio.com>
2014-10-14 00:44:32 -0700

    pdf render -- add test WIP

---
+++ pdf_render/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '1983d836d3c7538a57934a066d18f1a7050f143f5643a71a85d378f7987b77d81fc5a6387fe554f0197ddc0be25a6b110c7750fc9000fd4f9a0de8b74ec389cf'
